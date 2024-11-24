2a4b965d2f7bb713c8fdc3b7dfede6189f7e0e7d
Eriikakl <eriika.klenberg@myy.haaga-helia.fi>
2024-11-24 22:28:06 +0200

    Added Profile screen to Bottom tabs

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import Entypo from '@expo/vector-icons/Entypo';
 import { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import HomeScreen from './screens/HomeScreen'
 import ClubsStackScreen from './screens/ClubStack';
 import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import { UserProvider } from './components/UserContext';
+import { Provider as PaperProvider } from 'react-native-paper'
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -30,57 +33,77 @@ export default function App() {
   }, []);
 
   return (
-    <UserProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {!isLoggedIn ? ( // Tarkistetaan kirjautumistila
-            <Stack.Screen name="Login" options={{ headerShown: false }}>
-              {() => <LoginScreen onLogin={() => setIsLoggedIn(true)} />}
-            </Stack.Screen>
-          ) : (
-            <Stack.Screen name="Main" options={{ headerShown: false }}>
-              {() => (
-                <Tab.Navigator
-                  screenOptions={({ route }) => ({
-                    tabBarIcon: ({ color, size }) => {
-                      let iconName;
+    <PaperProvider>
+      <UserProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            {!isLoggedIn ? ( // Tarkistetaan kirjautumistila
+              <Stack.Screen name="Login" options={{ headerShown: false }}>
+                {() => <LoginScreen onLogin={() => setIsLoggedIn(true)} />}
+              </Stack.Screen>
+            ) : (
+              <Stack.Screen name="Main" options={{ headerShown: false }}>
+                {() => (
+                  <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                      tabBarIcon: ({ color, size }) => {
+                        let iconName;
 
-                      if (route.name === "Home") {
-                        iconName = "home";
-                      } else if (route.name === "Clubs") {
-                        iconName = "book";
-                      }
+                        if (route.name === "Home") {
+                          iconName = "home";
+                        } else if (route.name === "Clubs") {
+                          iconName = "book";
+                        } else if (route.name === "Profile") {
+                          iconName = "user";
+                          return <Entypo name="user" size={size} color={color} />;
+                        }
 
-                      return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                    tabBarActiveTintColor: '#695F5F',
-                    tabBarInactiveTintColor: 'lightgray',
-                  })}>
-                  <Tab.Screen
-                    name="Home"
-                    component={HomeScreen}
-                    options={{
-                      headerShown: true,
-                      headerStyle: {
-                        backgroundColor: "#cfbaba",
+
+                        return <Ionicons name={iconName} size={size} color={color} />
                       },
-                      headerTintColor: "black",
-                      headerTitleStyle: {
-                        fontWeight: 'bold',
-                      },
-                    }}
-                  />
-                  <Tab.Screen
-                    name="Clubs"
-                    component={ClubsStackScreen}
-                    options={{ headerShown: false }}
-                  />
-                </Tab.Navigator>
-              )}
-            </Stack.Screen>
-          )}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </UserProvider>
+                      tabBarActiveTintColor: '#695F5F',
+                      tabBarInactiveTintColor: 'lightgray',
+                    })}>
+                    <Tab.Screen
+                      name="Home"
+                      component={HomeScreen}
+                      options={{
+                        headerShown: true,
+                        headerStyle: {
+                          backgroundColor: "#cfbaba",
+                        },
+                        headerTintColor: "black",
+                        headerTitleStyle: {
+                          fontWeight: 'bold',
+                        },
+                      }}
+                    />
+                    <Tab.Screen
+                      name="Clubs"
+                      component={ClubsStackScreen}
+                      options={{ headerShown: false }}
+                    />
+                    <Tab.Screen
+                      name="Profile"
+                      component={ProfileScreen}
+                      options={{
+                        headerShown: true,
+                        headerStyle: {
+                          backgroundColor: "#cfbaba",
+                        },
+                        headerTintColor: "black",
+                        headerTitleStyle: {
+                          fontWeight: 'bold',
+                        },
+                      }}
+                    />
+                  </Tab.Navigator>
+                )}
+              </Stack.Screen>
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </UserProvider>
+    </PaperProvider>
   );
 }
---
+++ screens/ProfileScreen.js
@@ -0,0 +1,77 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { useUser } from '../components/UserContext';
+import { useState, useEffect } from 'react';
+
+// yhteys projektiin
+import { app } from '../firebaseConfig';
+// yhteys projektin palveluihin
+import { getDatabase, ref, onValue } from 'firebase/database';
+
+const database = getDatabase(app);
+
+export default function ProfileScreen() {
+  const { user } = useUser();
+  const [clubNames, setClubNames] = useState([]);
+
+  // Haetaan käyttäjän klubit
+  useEffect(() => {
+    if (user && user.username) {
+      const clubRef = ref(database, `/users/${user.username}/following`);
+
+      const unsubscribe = onValue(clubRef, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          // Suodatetaan seuratut klubit, joilla arvo on `true`
+          const followedClubs = Object.keys(data).filter((clubName) => data[clubName] === true);
+          setClubNames(followedClubs); // Tallennetaan klubien nimet
+        } else {
+          setClubNames([]);
+        }
+      });
+
+      return () => unsubscribe();
+    }
+  }, [user]);
+
+  return (
+    <View style={styles.container}>
+      <View style={{ fontSize: 20, justifyContent: 'flex-end' }}>
+      <Text style={{ fontSize: 28, justifyContent: 'flex-start' }}>Seuraamasi lukupiirit:</Text>
+      {clubNames ? (
+        clubNames.length > 0 ? (
+          clubNames.map((club, index) => (
+            <View key={index} style={{
+              backgroundColor: '#695F5F',
+              padding: 20,
+              marginVertical: 8,
+              marginHorizontal: 16,
+              width: "300",
+              flexDirection: "row",
+              justifyContent: "center"
+      
+            }}>
+            <Text key={index}>{club}</Text>
+            </View>
+          ))
+        ) : (
+          <Text>Et seuraa yhtäkään lukupiiriä</Text>
+        )
+      ) : (
+        <Text>Ladataan seuraamiasi lukupiirejä...</Text>
+      )}
+    </View>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    backgroundColor: '#fafaf7',
+    justifyContent:'flex-start',
+    alignItems: 'center',
+    padding: 20,
+  },
+ 
+});
